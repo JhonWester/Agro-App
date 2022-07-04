1d335fda24cde8703f4233c56b4f85a4be7e0b3a
Jhon James Hernandez <Jhon Wester>
2022-07-04 12:29:22 -0500

    Logic required to create user and add user service

---
+++ src/app/Services/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { User } from '../Models/Class/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  _user: User;
+
+  constructor() { }
+
+  get User(): User {
+    return this._user;
+  }
+ 
+  set User(user: User) {
+    this._user = user;
+  }
+}
