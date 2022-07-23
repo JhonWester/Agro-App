07f1e25afbef4ab7a08bf6dca0dc4b05bc3f1932
Jhon James Hernandez <Jhon Wester>
2022-07-23 11:12:31 -0500

    Add information to the page

--- src/app/components/init/section/section.component.ts
+++ src/app/components/init/section/section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section',
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class SectionComponent implements OnInit {
 
   constructor() { }
+
+  @Input() Information: string;
+  @Input() tittle: string;
 
   ngOnInit(): void {
   }
